91005860622f8e78bd39a1939e2123f86b272ba3
Itai Schwed <itai.schwed@gmail.com>
2019-07-21 16:35:34 +0300

    add to ex2

--- ex2-210719.js
+++ ex2-210719.js
@@ -20,10 +20,10 @@ function CreateMobilePhone(model,size,price,number){
 
 //ctor mode
 function MobilePhone(model,size,price,number){
-    this.model = model;
     this.size = size;
-    this.price = price;
     this.number = number;
+    let _price  = 0;
+    let _model  = "";
     
     this.getNumber = function(){
         return this.number;
@@ -31,10 +31,34 @@ function MobilePhone(model,size,price,number){
     this.setNUmber = function(number){
         this.number = number;
     }
+
+    this.getPrice = () => {
+        return _price;
+    }
+    this.setPrice = (newPrice) => {
+        if(typeof newPrice === "number" && newPrice === parseInt(newPrice,10) && newPrice > _price){
+            _price = newPrice;
+        }
+    }
+
+    this.getModel = () => {
+        return _model;
+    }
+
+    this.setModel = (newModel) => {
+        if(typeof newModel === "string" && newModel.length >= 4){
+            _model = newModel;
+        }
+    }
+
+    this.setModel(model);
+    this.setPrice(price);
 }
 
 let phone1 = CreateMobilePhone("model1","size1","price1","number1");
-let phone2 = new MobilePhone("model2","size2","price2","number2");
+let phone2 = new MobilePhone("model2","size2",10,"number2");
 
 console.log(phone1);
 console.log(phone2);
+console.log(phone2.getModel());
+console.log(phone2.getPrice());
