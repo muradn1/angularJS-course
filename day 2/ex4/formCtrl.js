d25609c76f238d543cf8ba4fb72d3a7bddb351c8
Itai Schwed <itai.schwed@gmail.com>
2019-07-22 16:31:09 +0300

    add ex4

---
+++ day 2/ex4/formCtrl.js
@@ -0,0 +1,34 @@
+
+// also works
+// angular.module("myTimerApp").controller("timeCtrl", () => { alert("hello from time controller")})
+
+//module.controller("timeCtrl", () => { alert("hello from time controller")})
+
+module.controller("formCtrl", FormCtrl)
+
+
+// DI dependency injection - IOC
+function FormCtrl($scope) {
+    $scope.person = new Person();
+    
+    $scope.resetName = function() {
+        $scope.person.name='';  
+    }
+
+}
+
+class Person {
+    constructor(name, age, password, gender, vehicleKind, haveBike) {
+        this.name = name;
+        this.age = age;
+        this.password = password;
+        this.gender = gender;
+        this.vehicleKind = vehicleKind;
+        this.haveBike = haveBike;
+    }
+
+    toString() {
+        const { name, age, password, gender, vehicleKind, haveBike } = this;
+        return `name: ${name}, age: ${age}, password: ${password}, gender: ${gender}, vehicleKind: ${vehicleKind}, haveBike: ${haveBike}`;
+    }
+}
